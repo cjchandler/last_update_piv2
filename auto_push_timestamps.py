33b7729e0db6f82da2b366a1bfeacc4ea1b4d869
Carl <carljosephchandler@gmail.com>
2026-09-18 11:23:35 -0300

    looped

--- auto_push_timestamps.py
+++ auto_push_timestamps.py
@@ -31,27 +31,27 @@ def push_latest_timestamp( tval ):
    
 #send a message saysing all's well, I'm alive, here's the latest timestamp
 while True: 
-	try: 
-		#look up the file:
-		filepath = "/home/carl/Git_Projects/incubator/incubator/pi_incubator/datalog/today_data_piV2.csv"
-		#look at the pandas thing for last timestamp
-		df = pd.read_csv(filepath)
-		print(df.tail(20))
-		tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
-		
-		now = datetime.now()
-		# Extract integers
-		current_hour = now.hour
-		current_minute = now.minute
-		print( current_hour , current_minute)
-		if current_hour == 12 and current_minute < 10:
-			pass
-		else:
-			push_latest_timestamp( tsaved  )
+    try: 
+        #look up the file:
+        filepath = "/home/carl/Git_Projects/incubator/incubator/pi_incubator/datalog/today_data_piV2.csv"
+        #look at the pandas thing for last timestamp
+        df = pd.read_csv(filepath)
+        print(df.tail(20))
+        tsaved = df[df.columns[2]].iloc[-1] #2 is the last time saved column
+        
+        now = datetime.now()
+        # Extract integers
+        current_hour = now.hour
+        current_minute = now.minute
+        print( current_hour , current_minute)
+        if current_hour == 12 and current_minute < 10:
+            pass
+        else:
+            push_latest_timestamp( tsaved  )
 
-	except Exception as e:
-		print(e)
-		
-	tiem.sleep(2*60)
+    except Exception as e:
+        print(e)
+        
+    time.sleep(2*60)
     
     
